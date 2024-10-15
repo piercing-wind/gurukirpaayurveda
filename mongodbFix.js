03d694a76d435990119d85339e3ab1f79e258a7f
Sourabh Sharma <sourabhsharma5677@gmail.com>
2024-10-16 00:41:50 +0530

    Minor Changes

--- mongodbFix.js
+++ mongodbFix.js
@@ -6,31 +6,23 @@
 
 
 const { v4: uuidv4 } = require('uuid');
+
 const MongoClient = require('mongodb').MongoClient;
 
-// async function updateGatewayOrderIds() {
-//    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
- 
-//    try {
-//      await client.connect();
-//      const database = client.db('gurukirpa_ayurveda_database');
-//      const collection = database.collection('Order');
- 
-//      const cursor = collection.find({ gateway_order_id: { $in: [null, ''] } });
- 
-//      while (await cursor.hasNext()) {
-//        const doc = await cursor.next();
-//        const newGatewayOrderId = uuidv4();
-//        await collection.updateOne(
-//          { _id: doc._id },
-//          { $set: { gateway_order_id: newGatewayOrderId } }
-//        );
-//      }
- 
-//      console.log('Updated all records with unique gateway_order_id');
-//    } finally {
-//      await client.close();
-//    }
-//  }
- 
-//  updateGatewayOrderIds().catch(console.error);
+async function deleteAllOrders() {
+   const uri = 'mongodb+srv://gurukirpa:EwtNBWU9Iek0HpdB@gurukirpa-ayurveda-clus.wzocq.mongodb.net/gurukirpa_ayurveda_database?retryWrites=true&w=majority&appName=gurukirpa-ayurveda-cluster'; // Replace with your MongoDB connection string
+   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+   try {
+      await client.connect();
+      const database = client.db('gurukirpa_ayurveda_database');
+      const collection = database.collection('Order');
+
+      const result = await collection.deleteMany({});
+      console.log(`Deleted ${result.deletedCount} records from the Order collection`);
+   } finally {
+      await client.close();
+   }
+}
+
+deleteAllOrders().catch(console.error);
