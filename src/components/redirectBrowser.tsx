82ca3c0df98bf6c7081e225a55b376ced5ea7161
Sourabh Sharma <sourabhsharma5677@gmail.com>
2024-11-09 21:47:33 +0530

    Browser Detection

--- src/components/redirectBrowser.tsx
+++ src/components/redirectBrowser.tsx
@@ -21,30 +21,49 @@ const useRedirectToChrome = () => {
       return inAppBrowserPatterns.some((pattern) => pattern.test(userAgent));
     };
 
+    // Function to check if the website is in an iframe and the parent domain is byteswithbits.com
+    const isInIframeFromBytesWithBits = () => {
+      try {
+        return window.self !== window.top && document.referrer.includes('byteswithbits.com');
+      } catch (e) {
+        return false;
+      }
+    };
+
     // Check if the user is on Android
     if (/android/i.test(userAgent)) {
       if (isInAppBrowser()) {
-        // Attempt to open in Chrome using intent
-        window.location.href = "intent://" + window.location.href.replace(/^https?:\/\//, '') + "#Intent;scheme=https;package=com.android.chrome;end;";
-        setTimeout(() => {
-         // If no browser is found, redirect to the Play Store to install Chrome
-         window.location.href = "https://play.google.com/store/apps/details?id=com.android.chrome";
-       }, 500);
+        if (isInIframeFromBytesWithBits()) {
+          // Disable error handling for byteswithbits.com
+         //  console.log('Disabling error handling for byteswithbits.com');
+        } else {
+          // Attempt to open in Chrome using intent
+          window.location.href = "intent://" + window.location.href.replace(/^https?:\/\//, '') + "#Intent;scheme=https;package=com.android.chrome;end;";
+          setTimeout(() => {
+            // If no browser is found, prompt the user to choose from other browsers
+            window.location.href = "intent://" + window.location.href.replace(/^https?:\/\//, '') + "#Intent;scheme=https;end;";
+          }, 1000);
+        }
       }
     } 
     // Check if the user is on iOS
     else if (/iPad|iPhone|iPod/.test(userAgent)) {
       if (isInAppBrowser()) {
-        // Attempt to open in Safari
-        window.location.href = window.location.href;
+        if (isInIframeFromBytesWithBits()) {
+          // Disable error handling for byteswithbits.com
+         //  console.log('Disabling error handling for byteswithbits.com');
+        } else {
+          // Attempt to open in Safari
+          window.location.href = window.location.href;
+        }
       }
     }
   }, []);
 };
 
 const RedirectWrapper = () => {
-   useRedirectToChrome();
-   return null;
- };
+  useRedirectToChrome();
+  return null;
+};
 
 export default RedirectWrapper;
